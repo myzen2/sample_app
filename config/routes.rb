1d8088dbfe91cdb804164e4910cecbb46f0cddca
myzen2 <mehdi.zenbil@orange.fr>
2015-11-17 16:10:31 +0100

    ajout des pages User et gestion des sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root             'pages#home'
   get 'help'    => 'pages#help'
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
 
   get 'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
